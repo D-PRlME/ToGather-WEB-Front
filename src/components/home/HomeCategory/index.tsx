5fb75ae63bdeffd2002e850d811c9458dd9f718d
jisoung <jangjisoung0405@gmail.com>
2022-10-29 04:29:18 +0900

    refactor : 스타일 파일 리팩토링

--- src/components/home/HomeCategory/index.tsx
+++ src/components/home/HomeCategory/index.tsx
@@ -1,14 +1,5 @@
 import React from "react";
-import {
-  HomeCategoryContainer,
-  HomeLogoBlock,
-  HomePageLogo,
-  HomeText,
-  MenuBlock,
-  MenuLogo,
-  MenuText,
-  ProfileBlock,
-} from "./style";
+import * as _ from "./style";
 import Logo from "../../../assets/logo/logo.svg";
 import HomeLogo from "../../../assets/logo/home.svg";
 import SearchLogo from "../../../assets/logo/search.svg";
@@ -17,28 +8,28 @@ import ProfileLogo from "../../../assets/logo/profile.svg";
 
 const HomeCategory = () => {
   return (
-    <HomeCategoryContainer>
-      <HomeLogoBlock>
-        <HomePageLogo src={Logo}></HomePageLogo>
-        <HomeText>ToGather</HomeText>
-      </HomeLogoBlock>
-      <MenuBlock>
-        <MenuLogo src={HomeLogo} />
-        <MenuText>홈</MenuText>
-      </MenuBlock>
-      <MenuBlock>
-        <MenuLogo src={SearchLogo} />
-        <MenuText>검색</MenuText>
-      </MenuBlock>
-      <MenuBlock>
-        <MenuLogo src={PostLogo} />
-        <MenuText>글쓰기</MenuText>
-      </MenuBlock>
-      <ProfileBlock>
-        <MenuLogo src={ProfileLogo} />
-        <MenuText>나</MenuText>
-      </ProfileBlock>
-    </HomeCategoryContainer>
+    <_.HomeCategoryContainer>
+      <_.HomeLogoBlock>
+        <_.HomePageLogo src={Logo}></_.HomePageLogo>
+        <_.HomeText>ToGather</_.HomeText>
+      </_.HomeLogoBlock>
+      <_.MenuBlock>
+        <_.MenuLogo src={HomeLogo} />
+        <_.MenuText>홈</_.MenuText>
+      </_.MenuBlock>
+      <_.MenuBlock>
+        <_.MenuLogo src={SearchLogo} />
+        <_.MenuText>검색</_.MenuText>
+      </_.MenuBlock>
+      <_.MenuBlock>
+        <_.MenuLogo src={PostLogo} />
+        <_.MenuText>글쓰기</_.MenuText>
+      </_.MenuBlock>
+      <_.ProfileBlock>
+        <_.MenuLogo src={ProfileLogo} />
+        <_.MenuText>나</_.MenuText>
+      </_.ProfileBlock>
+    </_.HomeCategoryContainer>
   );
 };
 
