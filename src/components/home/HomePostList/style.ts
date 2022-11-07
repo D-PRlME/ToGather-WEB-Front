4a8b322fd0d80e820e75b156ccfd14617704fe06
jisoung <jangjisoung0405@gmail.com>
2022-11-07 19:14:24 +0900

    Style : 스타일 수정

--- src/components/home/HomePostList/style.ts
+++ src/components/home/HomePostList/style.ts
@@ -2,43 +2,43 @@ import styled from "styled-components";
 
 export const HomePostSkillHeader = styled.div`
   display: flex;
+  align-items: center;
   width: 785px;
-  height: 150px;
-  margin: 10px;
+  height: 200px;
   margin-left: 20px;
-  padding: 4px;
+  padding-left: 20px;
   border-radius: 8px;
   background: #f7f7f7;
   overflow-x: scroll;
   overflow-y: hidden;
+  gap: 15px;
+  margin-bottom: 5px;
 `;
 
 export const HomePostListContainer = styled.div`
   width: auto;
   height: 900px;
-display: flex;
+  display: flex;
   flex-direction: column;
-  // background-color: yellow;
+  padding: 20px;
+   //background-color: yellow;
 `;
 
 export const Skill = styled.div`
-  width: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   height: 80px;
   // background-color: aqua;
   border-radius: 10px;
   // padding-top: 16px;
-  margin: 8px;
 `;
 
 export const SkillLogo = styled.img`
-  padding-left: 5px;
   width: 50px;
   height: 50px;
 `;
 
 export const SkillText = styled.div`
-  padding-left: 10px;
-
-  width: 10px;
   height: 20px;
 `;
