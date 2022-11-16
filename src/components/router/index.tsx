3810aa6d3739257a1bbe81f1d8008fffd8a7d340
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2022-11-16 20:12:21 +0900

    fix : 몇 가지 오류 수정

--- src/components/router/index.tsx
+++ src/components/router/index.tsx
@@ -2,25 +2,27 @@ import { Route, Routes as Switch } from "react-router-dom";
 
 import PostsPage from "../../pages/PostPage";
 import HomePage from "../../pages/HomePage";
+import SignUpPage from "../../pages/SignupPage";
 import LogInPage from "../../pages/LogInPage";
 import MyPage from "../../pages/MyPage";
 import SearchPage from "../../pages/SearchPage";
 import CreatePage from "../../pages/CreatePage";
-import EditPage from "../../pages/EditPage"
-
+import EditPage from "../../pages/EditPage";
+import React from "react";
 
 const Router = () => {
   return (
     <Switch>
       <Route path="/" element={<HomePage />} />
+      <Route path="/signup" element={<SignUpPage />} />
       <Route path="/login" element={<LogInPage />} />
       <Route path="/edit" element={<EditPage />} />
       <Route path="/mypage/*" element={<MyPage />}></Route>
 
       <Route path="/create" element={<CreatePage />} />
-      <Route path="/edit/*" element={<EditPage/>}/>
+      <Route path="/edit/*" element={<EditPage />} />
       {/*TODO : 라우터 하나로 통일해야함*/}
-      <Route path="/mypage/*" element={<MyPage />}/>
+      <Route path="/mypage/*" element={<MyPage />} />
       <Route path="/posts/*" element={<PostsPage />} />
       <Route path="/search" element={<SearchPage />} />
     </Switch>
