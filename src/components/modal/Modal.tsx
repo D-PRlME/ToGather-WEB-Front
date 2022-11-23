96bc99690763a587ef89af26a0dd04f007f87872
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2022-11-23 20:33:52 +0900

    feat : 로그아웃 모달창 완성

---
+++ src/components/modal/Modal.tsx
@@ -0,0 +1,27 @@
+import React, { PropsWithChildren } from "react";
+import { Backdrop, DialogBox, ModalContainer } from "./style";
+interface ModalDefaultType {
+  onClickToggleModal: () => void;
+}
+
+function Modal({
+  onClickToggleModal,
+  children,
+}: PropsWithChildren<ModalDefaultType>) {
+  return (
+    <ModalContainer>
+      <DialogBox>{children}</DialogBox>
+      <Backdrop
+        onClick={(e: React.MouseEvent) => {
+          e.preventDefault();
+
+          if (onClickToggleModal) {
+            onClickToggleModal();
+          }
+        }}
+      />
+    </ModalContainer>
+  );
+}
+
+export default Modal;
